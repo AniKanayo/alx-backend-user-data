e4fe823e564282c9729b32185cbaeeb02254adf3
AniKanayo <micanian4real@gmail.com>
2023-08-09 16:18:21 +0000

    Personal Tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,76 +9,89 @@ from typing import List
 import logging
 
 
+class SensitiveDataFilter(logging.Filter):
+    """
+    Custom logging filter to redact sensitive information from log messages.
+    """
+
+    SENSITIVE_FIELDS = [
+        "password",
+        "date_of_birth"
+    ]
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        """
+        Filter method to redact sensitive information from log messages.
+
+        Args:
+            record: The LogRecord to be filtered.
+
+        Returns:
+            True to allow the record to be processed, False to discard it.
+        """
+        message = record.getMessage()
+        for field in self.SENSITIVE_FIELDS:
+            message = re.sub(fr"{field}=([^;]*)", f"{field}=<REDACTED>",
+                             message)
+        record.msg = message
+        return True
+
+
 class RedactingFormatter(logging.Formatter):
     """
-    Class RedactingFormatter
-
-    Redacting Formatter class for filtering sensitive
-    information in log messages.
+    Custom log formatter to filter and redact sensitive information
+    in log messages.
     """
-    REDACTION = "***"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__("[HOLBERTON] \
-            %(name)s %(levelname)s %(asctime)-15s: %(message)s")
-        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
-        Format function to filter sensitive information
-        in log messages.
+        Format method to filter and redact sensitive information in log
+        messages.
 
         Args:
             record: The LogRecord to be formatted.
 
         Returns:
-            The formatted log message with filtered sensitive
-            information.
+            The formatted log message with filtered sensitive information.
         """
-        message = self.filter_fields(record.msg)
-        record.msg = message
+        if isinstance(record.msg, str):
+            record.msg = self.redact(record.msg)
         return super().format(record)
 
-    def filter_fields(self, message: str) -> str:
+    def redact(self, message: str) -> str:
         """
-        Filter and redact specific fields in a log message.
+        Redact sensitive information in the log message.
 
         Args:
-            message: The log message to be filtered.
+            message: The log message to be redacted.
 
         Returns:
-            The filtered log message with redacted values for the
-            specified fields.
+            The redacted log message.
         """
-        for field in self.fields:
-            pattern = fr"{field}=([^;]*)"
-            message = re.sub(pattern, fr"{field}={self.REDACTION}", message)
-        return message
+        filter = SensitiveDataFilter()
+        return filter.filter(message)
 
 
 def main() -> None:
     """
-    Main function to demonstrate the usage of RedactingFormatter
-    class.
+    Main function to demonstrate the usage of RedactingFormatter class.
     """
-    # Create logger
-    logger = logging.getLogger("my_logger")
-    logger.setLevel(logging.INFO)
+    formatter = RedactingFormatter("[HOLBERTON] %(name)s \
+        %(levelname)s %(asctime)-15s: %(message)s")
+    logger = logging.getLogger("example")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
-    # Create formatter
-    formatter = RedactingFormatter(fields=["email", "ssn", "password"])
+    messages = [
+        "name=egg;email=eggmin@eggsample.com;password=secret;"
+        "date_of_birth=01/01/2000;",
+        "name=bob;email=bob@dylan.com;password=123456;"
+        "date_of_birth=02/02/2002;",
+    ]
 
-    # Create console handler and set formatter
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-
-    # Add console handler to the logger
-    logger.addHandler(console_handler)
-
-    # Log a message
-    logger.info("name=Bob; email=bob@example.com; ssn=123-45-6789;"
-                " password=secret")
+    for message in messages:
+        logger.warning(message)
 
 
 if __name__ == "__main__":
