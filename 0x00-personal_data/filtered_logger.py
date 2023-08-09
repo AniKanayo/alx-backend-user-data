f8aaf54837598c5988c2d9e729a1e2efc996c6df
AniKanayo <micanian4real@gmail.com>
2023-08-09 11:02:31 +0000

    Personal Tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,11 +3,12 @@
 import re
 from typing import List, Any
 
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
     Replaces field values in the given message with a redaction string.
-    
+
     Args:
         fields (List[str]): a list of strings representing the fields to be
         obfuscated.
@@ -22,14 +23,5 @@ def filter_datum(fields: List[str], redaction: str,
     """
     for field in fields:
         message = re.sub(f"{field}={field}[^{separator}]*",
-                       f"{field}={redaction}", message)
+                         f"{field}={redaction}", message)
     return message
-
-
-# Just for testing purpose
-fields: List[str] = ["password", "date_of_birth"]
-redaction: str = "[redacted]"
-message: str = "username=user1;password=1234;date_of_birth=01/01/2021"
-separator: str = ";"
-
-print(filter_datum(fields, redaction, message, separator))
