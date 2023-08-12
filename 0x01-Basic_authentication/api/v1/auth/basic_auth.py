03c27f5e04f38d1a4de931babbf4e2d7473fba41
AniKanayo <micanian4real@gmail.com>
2023-08-12 21:56:49 +0000

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,16 @@ class BasicAuth(Auth):
     """
     BasicAuth class to manage the API authentication.
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extract the Base64 part of the Authorization header
+        for Basic Authentication.
+        """
+        if authorization_header is None or not \
+                isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[1]
