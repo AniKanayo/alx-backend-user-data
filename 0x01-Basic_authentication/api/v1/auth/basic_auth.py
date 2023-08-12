a554b7448f2ee3107296846beac5143b8e016ea1
AniKanayo <micanian4real@gmail.com>
2023-08-12 21:14:33 +0000

    Basic Authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Auth module for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class to manage the API authentication.
+    """
+    pass
