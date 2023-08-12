17c00dc8278186ca49995d1ca43b1da9487b542a
AniKanayo <micanian4real@gmail.com>
2023-08-12 23:10:44 +0000

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Basic Auth module for the API
 """
 from api.v1.auth.auth import Auth
 from models.user import User
+import base64
+import binascii
 from typing import TypeVar
 
 
@@ -15,8 +17,8 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self, authorization_header:
                                             str) -> str:
         """
-        Extract the Base64 part of the Authorization header
-        for Basic Authentication.
+        Extract the Base64 part of the Authorization header for
+        Basic Authentication.
         """
         if authorization_header is None or not \
                 isinstance(authorization_header, str):
@@ -38,7 +40,7 @@ class BasicAuth(Auth):
             decoded_bytes = base64.b64decode(base64_authorization_header)
             decoded_string = decoded_bytes.decode('utf-8')
             return decoded_string
-        except base64.binascii.Error:
+        except binascii.Error:
             return None
 
     def extract_user_credentials(self, decoded_base64_authorization_header:
@@ -58,8 +60,8 @@ class BasicAuth(Auth):
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
         """
-        Return the User instance based on the provided
-        user email and password.
+        Return the User instance based on the provided user
+        email and password.
         """
         if user_email is None or not isinstance(user_email, str):
             return None
