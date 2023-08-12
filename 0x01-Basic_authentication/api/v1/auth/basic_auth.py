ff6937ba556723155d6812286d2ad6f972f1e0bf
AniKanayo <micanian4real@gmail.com>
2023-08-12 22:07:26 +0000

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Auth module for the API
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -10,8 +11,8 @@ class BasicAuth(Auth):
     BasicAuth class to manage the API authentication.
     """
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header:
+                                            str) -> str:
         """
         Extract the Base64 part of the Authorization header
         for Basic Authentication.
@@ -22,3 +23,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """
+        Decode the Base64 string provided in the
+        'base64_authorization_header' parameter.
+        """
+        if base64_authorization_header is None or not \
+                isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except base64.binascii.Error:
+            return None
