eff2ad866951c088bf22edfeb5b4d831990295a9
AniKanayo <micanian4real@gmail.com>
2023-08-12 22:49:58 +0000

    Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,8 @@
 Basic Auth module for the API
 """
 from api.v1.auth.auth import Auth
-import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -14,8 +15,8 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self, authorization_header:
                                             str) -> str:
         """
-        Extract the Base64 part of the Authorization header for
-        Basic Authentication.
+        Extract the Base64 part of the Authorization header
+        for Basic Authentication.
         """
         if authorization_header is None or not \
                 isinstance(authorization_header, str):
@@ -53,3 +54,24 @@ class BasicAuth(Auth):
         user_email, user_password = \
             decoded_base64_authorization_header.split(':')
         return user_email, user_password
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        Return the User instance based on the provided user email
+        and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({"email": user_email})
+        if len(users) == 0:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
