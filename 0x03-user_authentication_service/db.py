56da00d6f7bedc39881373d7e3a93c46d493bd01
AniKanayo <micanian4real@gmail.com>
2023-08-15 21:24:45 +0000

    user_authentication_service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
-"""DB module
 """
-from sqlalchemy import create_engine
+DB module
+"""
+
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
-from user import Base, User
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), nullable=False)
+    hashed_password = Column(String(120), nullable=False)
+
+    def __init__(self, email: str, hashed_password: str):
+        self.email = email
+        self.hashed_password = hashed_password
 
 
 class DB:
-    """DB class
-    """
-
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+    def __init__(self):
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -23,17 +33,28 @@ class DB:
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
-        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database and return the User object.
-        """
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        session = self._session
+        session.add(user)
+        session.commit()
         return user
+
+    def get_user(self, user_id: int) -> User:
+        try:
+            user = self._session.query(User).filter_by(id=user_id).one()
+            return user
+        except NoResultFound:
+            return None
+
+    def find_user_by(self, **kwargs) -> User:
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+            return user
+        except (NoResultFound, InvalidRequestError):
+            return None
