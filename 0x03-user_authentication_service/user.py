2f7fef37d1db5dbd7d22b40766d1e40bf6926a78
AniKanayo <micanian4real@gmail.com>
2023-08-15 08:51:48 +0000

    User authentication service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -17,28 +17,7 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False, unique=True)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
-
-    def __init__(self, email: str, hashed_password: str,
-                 session_id: str = None, reset_token: str = None):
-        """
-        Initialize a User object.
-
-        :param email: The user's email address.
-        :param hashed_password: The hashed password.
-        :param session_id: The user's session ID (optional).
-        :param reset_token: The password reset token (optional).
-        """
-        self.email = email
-        self.hashed_password = hashed_password
-        self.session_id = session_id
-        self.reset_token = reset_token
-
-    def __repr__(self):
-        """
-        Return a string representation of the User object.
-        """
-        return f"<User(id={self.id}, email={self.email})>"
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
