747694ac2a2d0615e1650354bc666c8f1699e1f8
AniKanayo <micanian4real@gmail.com>
2023-08-17 11:35:23 +0000

    user_authentication_service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,24 +4,12 @@
 DB module
 """
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String)
-    hashed_password = Column(String)
-
-    def __init__(self, email, hashed_password):
-        self.email = email
-        self.hashed_password = hashed_password
+from user import Base, User
 
 
 class DB:
@@ -42,9 +30,6 @@ class DB:
     def _session(self) -> Session:
         """
         Memoized session object
-
-        Note: This is a private property and should NEVER be used
-        from outside the DB class.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -52,8 +37,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """
-        Add a new user to the database
+        """Add a new user to the database
 
         Args:
             email (str): Email of the user
